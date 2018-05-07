7b4d22824ee238c1cdaee19941c774a0df442247
Chau Than <simplesoul.chau@gmail.com>
2018-05-07 17:18:40 +0700

    2018-05-07

--- forms/09-course-select.jsx
+++ forms/09-course-select.jsx
@@ -3,19 +3,21 @@ import PropTypes from 'prop-types';
 import ApiClient from './api/client.js'
 
 class CourseSelectComponent extends Component {
+  static displayName = " 09 Field Component Form";
+
   state = {
     department: null,
     course: null,
-    courses: null,
+    courses: [],
     _loading: false,
   }
 
-  componentWillMount(){
-    this.setState({ _loading: true });
-    ApiClient.getCourse("core").then((courses) => {
-      this.setState({ courses, _loading: false });
-    });
-  }
+  // componentWillMount() {
+  //   this.setState({ _loading: true });
+  //   ApiClient.getCourse("core").then((courses) => {
+  //     this.setState({ courses, _loading: false });
+  //   });
+  // }
 
 
   componentWillReceiveProps(nextProps) {
@@ -25,21 +27,72 @@ class CourseSelectComponent extends Component {
     })
   }
 
+  onSelectDepartment = (evt) => {
+    const department = evt.target.value;
+    const course = null;
 
-  render() {
+    this.setState({ department, course });
+
+    this.fetch(department);
+  }
+
+  onSelectCourse = (evt) => {
+    const course = evt.target.value;
+    this.setState({ course });
+  }
+
+  fetch = (department) => {
+    this.setState({ _loading: true });
+    ApiClient.getCourse(department).then((courses) => {
+      this.setState({ courses, _loading: false });
+    });
+  }
+
+  renderDepartmentSelect = () => {
+    return (
+      <select name="deparment" id="department" onChange={this.onSelectDepartment} value={this.state.department || ''} >
+        <option value="">Which department?</option>
+        <option value="core">Core</option>
+        <option value="electives">Electives</option>
+      </select>
+    );
+  }
+
+  renderCourseSelect = () => {
     if (this.state._loading) {
       return (
         <div>Loading</div>
       )
     }
 
+    if (!this.state.department || !this.state.courses.length) return <span />;
+
+    return (
+      <select
+        onChange={this.onSelectCourse}
+        value={this.state.course || ''}
+      >
+
+        {[
+          <option value='' key='course-none'>
+            Which course?
+          </option>,
+          ...this.state.courses.map((course, i) => (
+            <option value={course} key={i}>
+              {course}
+            </option>
+          )),
+        ]}
+      </select>
+    );
+  }
+
+  render() {
+
     return (
       <div>
-        Course
-        <ul>
-          {this.state.courses.map((course, i) => <li key={i}>{course}</li>)}
-
-        </ul>
+        <div>{this.renderDepartmentSelect()}</div>
+        <div>{this.renderCourseSelect()}</div>
       </div>
     );
   }
